2078ff51305e021321c82b5cb38b716b117a503d
0x0 <ghstoreinvesting@gmail.com>
2018-04-07 22:50:46 +0800

    Start adding form validation

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,7 +1,19 @@
-document.getElementById('submit').addEventListener('click', function() {	
-	saveFormData();
+var MIN_PASS_LENGTH = 6;
+
+// When form is being submitted
+document.getElementById('submit').addEventListener('click', function(e) {
+	// Check if it is a valid form submission
+	if (document.getElementById('password').value.length < MIN_PASS_LENGTH) {
+		e.preventDefault();
+		document.getElementById('form').innerHTML += '<p class="error mt-3">Password must be greater than 6 characters</p>';
+	} else {
+		saveFormData();
+	}
 });
 
+/**
+ * Store form data to be used on another page
+ */
 function saveFormData() {
 	localStorage.email = document.getElementsByName('email')[0].value;
 
