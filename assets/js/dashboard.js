a6d5506fc4f2a675bda86b25bfe576849f631e1f
0x0 <ghstoreinvesting@gmail.com>
2018-04-08 22:47:41 +0800

    Fix nav links

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -4,22 +4,24 @@ addProjects();
  * Display all projects saved in local storage
  */
 function addProjects() {
-	var projects = JSON.parse(localStorage.projects);
+	if (localStorage.projects) {
+		var projects = JSON.parse(localStorage.projects);
 
-	for (var i = 0; i < projects.length; i++) {
-		var html = `
-		 <a href="project_show.html">
-				<div class="project-item material-border mb-2 clickable">
-					<div class="project-item-image-container mr-2">
-						<img src="assets/images/placeholder.png" alt="Project logo" class="project-logo">
+		for (var i = 0; i < projects.length; i++) {
+			var html = `
+			 <a href="project_show.html">
+					<div class="project-item material-border mb-2 clickable">
+						<div class="project-item-image-container mr-2">
+							<img src="assets/images/placeholder.png" alt="Project logo" class="project-logo">
+						</div>
+						<div class="project-description">
+							<h3 class="heading-sm mt-1 mb-0">${projects[i].name}</h3>
+							<p>${projects[i].description}</p>
+						</div>
 					</div>
-					<div class="project-description">
-						<h3 class="heading-sm mt-1 mb-0">${projects[i].name}</h3>
-						<p>${projects[i].description}</p>
-					</div>
-				</div>
-			</a>
-		`;
-		document.getElementById('projects').innerHTML += html;
+				</a>
+			`;
+			document.getElementById('projects').innerHTML += html;
+		}
 	}
 }
