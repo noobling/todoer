31b7d9790988c8969e42556ba580eb9e74902d3a
0x0 <ghstoreinvesting@gmail.com>
2018-04-09 12:19:33 +0800

    Finish off the pages

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -2,12 +2,12 @@ var MIN_PASS_LENGTH = 6;
 
 // When form is being submitted
 document.getElementById('submit').addEventListener('click', function(e) {
+	saveFormData();
+	
 	// Check if it is a valid form submission
 	if (document.getElementById('password').value.length < MIN_PASS_LENGTH) {
 		e.preventDefault();
-		document.getElementById('form').innerHTML += '<p class="error mt-3">Password must be greater than 6 characters</p>';
-	} else {
-		saveFormData();
+		document.getElementById('password').insertAdjacentHTML('afterend','<p class="error">Password must be greater than 6 characters</p>');
 	}
 });
 
